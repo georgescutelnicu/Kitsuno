42c316d8f41f789739005c8ece847d7d5866045a
georgescutelnicu <george.v.scutelnicu@gmail.com>
2024-10-08 00:12:19 +0300

    Add gitignore and mvn properties

--- app.js
+++
@@ -1,9 +0,0 @@
-document.querySelector(".arrow-down").addEventListener("click", function(event) {
-    event.preventDefault();
-    const targetSection = document.querySelector("#target");
-        
-    targetSection.scrollIntoView({
-         behavior: "smooth"
-    });
-});
-
