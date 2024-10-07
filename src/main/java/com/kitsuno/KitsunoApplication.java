65c7465b73be879a06068e2c955f707c0a784bd3
georgescutelnicu <george.v.scutelnicu@gmail.com>
2024-10-07 23:46:44 +0300

    Set up Spring Boot project

---
+++ src/main/java/com/kitsuno/KitsunoApplication.java
@@ -0,0 +1,14 @@
+package com.kitsuno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class KitsunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitsunoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kitsuno/KitsunoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kitsuno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitsunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
