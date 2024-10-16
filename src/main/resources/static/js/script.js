8482d42656efbef10f69c61cae9ddac5f9d5222e
georgescutelnicu <george.v.scutelnicu@gmail.com>
2024-10-16 03:46:21 +0300

    Implement modal functionality for hiragana page
    - Add js function to create and display modal window with kana info
    - Replace the basic chart with interactive buttons
    - Enhance styling of modal window and chart buttons

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -30,6 +30,49 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
+// Dynamically creates a modal window displaying detailed information about a kana character
+function showInfo(character, romaji, audio, story, mnemonic, strokeOrder) {
+
+    const modalOverlay = document.createElement('div');
+    modalOverlay.className = 'modal';
+
+    const modalContent = document.createElement('div');
+    modalContent.className = 'modal-content';
+    modalContent.innerHTML = `
+            <h2>${character} (${romaji})</h2>
+            <button class="audio-button" data-audio="${audio}" onclick="playAudio(this.dataset.audio)">🔊</button>
+            <div class="flip-container">
+                <div class="flipper">
+                    <div class="front">
+                        <img src="${mnemonic}" alt="Mnemonic for ${character}">
+                        <p class="mnemonic-text">${story}</p>
+                    </div>
+                    <div class="back">
+                        <img src="${strokeOrder}" alt="Stroke Order for ${character}">
+                    </div>
+                </div>
+            </div>`;
+
+    modalOverlay.appendChild(modalContent);
+
+    document.body.appendChild(modalOverlay);
+    modalOverlay.style.display = 'flex';
+
+    requestAnimationFrame(() => {
+        modalContent.style.transform = 'scale(1)';
+    });
+
+    modalOverlay.addEventListener('click', function (e) {
+        if (e.target === modalOverlay) {
+            modalContent.style.transform = 'scale(0)';
+            setTimeout(() => {
+                document.body.removeChild(modalOverlay);
+            }, 300);
+        }
+    });
+}
+
+
 // Play audio in kana pages
 function playAudio(audioSource) {
     const audio = new Audio(audioSource);
