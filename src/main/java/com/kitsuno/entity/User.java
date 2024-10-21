325763b1eda814fee2443dc7d1da088c00e30639
georgescutelnicu <george.v.scutelnicu@gmail.com>
2024-10-21 06:00:54 +0300

    Add user entity
    - Create entity for the user table

---
+++ src/main/java/com/kitsuno/entity/User.java
@@ -0,0 +1,84 @@
+package com.kitsuno.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @NotBlank(message = "Username is mandatory")
+    @Size(min = 6, max = 18, message = "Username must be between 6 and 18 characters")
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Email should be valid")
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @NotBlank(message = "Password is mandatory")
+    @Size(min = 60, max = 60, message = "Encrypted password must have 60 characters")
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "enabled")
+    private boolean enabled = true;
+
+    public User() {
+    }
+
+    public User(String username, String email, String password, boolean enabled) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.enabled = enabled;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
