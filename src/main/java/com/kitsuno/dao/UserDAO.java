b88a714f2a2ee918ff02357283ed353a81889500
georgescutelnicu <george.v.scutelnicu@gmail.com>
2024-10-22 04:09:06 +0300

    Add interface and implementation for user DAO
    - Create user DAO interface to find users by email and username and to
      save the uer in the db
    - Implement user DAO class
    - Handled cases with no result by usint Optional

---
+++ src/main/java/com/kitsuno/dao/UserDAO.java
@@ -0,0 +1,13 @@
+package com.kitsuno.dao;
+
+import com.kitsuno.entity.User;
+
+import java.util.Optional;
+
+
+public interface UserDAO {
+
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    User save(User user);
+}
---
+++ src/main/java/com/kitsuno/dao/UserDAOImpl.java
@@ -0,0 +1,59 @@
+package com.kitsuno.dao;
+
+import com.kitsuno.entity.User;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public class UserDAOImpl implements UserDAO {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Autowired
+    public UserDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public Optional<User> findByUsername(String username) {
+        TypedQuery<User> query = entityManager.createQuery("SELECT u FROM User u WHERE u.username = :username", User.class);
+
+        query.setParameter("username", username);
+
+        try {
+            User user = query.getSingleResult();
+            return Optional.of(user);
+        } catch (NoResultException e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        TypedQuery<User> query = entityManager.createQuery("SELECT u FROM User u WHERE u.email = :email", User.class);
+        query.setParameter("email", email);
+
+        try {
+            User user = query.getSingleResult();
+            return Optional.of(user);
+        } catch (NoResultException e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    @Transactional
+    public User save(User user) {
+        entityManager.persist(user);
+
+        return user;
+    }
+}
