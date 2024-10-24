5090c3d807a38bddb3887ce6fb9ffd36bffe4fc1
georgescutelnicu <george.v.scutelnicu@gmail.com>
2024-10-24 23:59:09 +0300

    Add entity for the flashcard table

---
+++ src/main/java/com/kitsuno/entity/Flashcard.java
@@ -0,0 +1,78 @@
+package com.kitsuno.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "flashcards")
+public class Flashcard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "vocabulary")
+    private String[] vocabulary;
+
+    @Column(name = "notes")
+    private String notes;
+
+    @ManyToOne
+    @JoinColumn(name = "kanji_id", nullable = false)
+    private Kanji kanji;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public Flashcard() {
+    }
+
+    public Flashcard(int id, String[] vocabulary, String notes, Kanji kanji, User user) {
+        this.id = id;
+        this.vocabulary = vocabulary;
+        this.notes = notes;
+        this.kanji = kanji;
+        this.user = user;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String[] getVocabulary() {
+        return vocabulary;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public Kanji getKanji() {
+        return kanji;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setVocabulary(String[] vocabulary) {
+        this.vocabulary = vocabulary;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public void setKanji(Kanji kanji) {
+        this.kanji = kanji;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
